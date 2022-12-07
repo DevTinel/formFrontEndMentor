e012cf9cb9709df5317c68cc319b81a18a416834
DevTinel <davidaugustolol36@gmail.com>
2022-12-07 14:16:59 -0300

    step 2

--- src/components/cardPlan/CardPlan.js
+++ src/components/cardPlan/CardPlan.js
@@ -1,12 +1,13 @@
 import styles from './CardPlan.module.css';
 
-export const CardPlan = ({ img, title, price }) => {
+export const CardPlan = ({ img, title, price, description }) => {
   return (
     <button className={styles.containerCard}>
       <img src={img} alt={title} />
       <div className={styles.descriptionCard}>
         <b>{title}</b>
         <span>{price}</span>
+        <span className={styles.description}>{description}</span>
       </div>
     </button>
   );
--- src/components/step2/Step2.js
+++ src/components/step2/Step2.js
@@ -1,26 +1,64 @@
-import React, { useState } from 'react';
 import { CardPlan } from '../cardPlan/CardPlan';
 import styles from './Step2.module.css';
 import Arcade from '../../assets/images/icon-arcade.svg';
 import Advanced from '../../assets/images/icon-advanced.svg';
 import Pro from '../../assets/images/icon-pro.svg';
+import { useState } from 'react';
 
 export const Step2 = () => {
+  const [toggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle(!toggle);
+  }
+
   return (
     <section className={styles.containerStep2}>
       <h1>Select your plan</h1>
       <span>You have the option of monthly or yearly billing</span>
-      <div className={styles.containerCards}>
-        <CardPlan img={Arcade} title="Arcade" price="$9/mo" />
-        <CardPlan img={Advanced} title="Advanced" price="$12/mo" />
-        <CardPlan img={Pro} title="Pro" price="$15/mo" />
-      </div>
+      {toggle ? (
+        <div className={styles.containerCards}>
+          <CardPlan img={Arcade} title="Arcade" price="$9/mo" description="" />
+          <CardPlan
+            img={Advanced}
+            title="Advanced"
+            price="$12/mo"
+            description=""
+          />
+          <CardPlan img={Pro} title="Pro" price="$15/mo" description="" />
+        </div>
+      ) : (
+        <div className={styles.containerCards}>
+          <CardPlan
+            img={Arcade}
+            title="Arcade"
+            price="$90/yr"
+            description="2 months free"
+          />
+          <CardPlan
+            img={Advanced}
+            title="Advanced"
+            price="$120/yr"
+            description="2 months free"
+          />
+          <CardPlan
+            img={Pro}
+            title="Pro"
+            price="$150/yr"
+            description="2 months free"
+          />
+        </div>
+      )}
       <div className={styles.plan}>
-        <p className={styles.active}>Monthly</p>
-        <button>
-          <div id="bol"></div>
+        <p className={toggle ? `${styles.active}` : ''}>Monthly</p>
+        <button onClick={handleToggle}>
+          <div
+            className={
+              toggle ? `${styles.activeMonth} ` : `${styles.activeYearly}`
+            }
+          ></div>
         </button>
-        <p>Yearly</p>
+        <p className={toggle ? '' : `${styles.active}`}>Yearly</p>
       </div>
     </section>
   );
